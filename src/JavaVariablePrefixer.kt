7961962e2f5f9c8e9e926e0d93f631a5a316b170
cptbronzebeard <omg4itep@gmail.com>
2019-12-16 01:37:52 +0300

    Code cleanup

---
+++ src/JavaVariablePrefixer.kt
@@ -0,0 +1,29 @@
+import com.intellij.codeInsight.completion.AllClassesGetter
+import com.intellij.codeInsight.completion.PlainPrefixMatcher
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.psi.JavaRecursiveElementVisitor
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiLocalVariable
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.refactoring.RefactoringFactory
+import com.intellij.util.Processor
+import processors.GlobalRenamingProcessor
+
+
+class JavaVariablePrefixer : AnAction("Prefix Java classes") {
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.getData(PlatformDataKeys.PROJECT)
+        val processor = GlobalRenamingProcessor()
+        AllClassesGetter.processJavaClasses(
+                PlainPrefixMatcher(""),
+                project!!,
+                GlobalSearchScope.projectScope(project),
+                processor
+        )
+    }
+
+}
+
--- src/processors/GlobalRenamingProcessor.kt
+++ src/processors/GlobalRenamingProcessor.kt
@@ -1,7 +1,6 @@
 package processors
 
 import com.intellij.psi.JavaRecursiveElementVisitor
-import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiLocalVariable
 import com.intellij.refactoring.RefactoringFactory
@@ -9,11 +8,10 @@ import com.intellij.util.Processor
 
 class GlobalRenamingProcessor : Processor<PsiElement> {
     override fun process(p0: PsiElement?): Boolean {
-        val begin = p0?.containingFile
-        begin?.accept(object : JavaRecursiveElementVisitor() {
+        p0?.containingFile?.accept(object : JavaRecursiveElementVisitor() {
             override fun visitLocalVariable(variable: PsiLocalVariable) {
                 super.visitLocalVariable(variable)
-                val r = RefactoringFactory.getInstance(p0.project).createRename(variable, "it_${variable.name}").run()
+                RefactoringFactory.getInstance(p0.project).createRename(variable, "it_${variable.name}").run()
             }
         })
         return true
