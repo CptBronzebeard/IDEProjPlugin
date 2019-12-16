b08e5a06d9d5bae56830a26e6316233b4bc5219a
cptbronzebeard <omg4itep@gmail.com>
2019-12-16 21:47:49 +0300

    Compile-time constant renaming added

--- src/processors/GlobalRenamingProcessor.kt
+++ src/processors/GlobalRenamingProcessor.kt
@@ -2,9 +2,11 @@ package processors
 
 import com.intellij.psi.JavaRecursiveElementVisitor
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiField
 import com.intellij.psi.PsiLocalVariable
 import com.intellij.refactoring.RefactoringFactory
 import com.intellij.util.Processor
+import com.intellij.psi.util.PsiUtil.*
 
 class GlobalRenamingProcessor : Processor<PsiElement> {
     override fun process(p0: PsiElement?): Boolean {
@@ -12,6 +14,11 @@ class GlobalRenamingProcessor : Processor<PsiElement> {
             override fun visitLocalVariable(variable: PsiLocalVariable) {
                 super.visitLocalVariable(variable)
                 RefactoringFactory.getInstance(p0.project).createRename(variable, "it_${variable.name}").run()
+            }
+
+            override fun visitField(field: PsiField) {
+                super.visitField(field)
+                if (isCompileTimeConstant(field)) RefactoringFactory.getInstance(p0.project).createRename(field, "c_${field.name}").run()
             }
         })
         return true
